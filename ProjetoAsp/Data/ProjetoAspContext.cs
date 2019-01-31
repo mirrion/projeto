dbd1ab0582a01c8f6cf3799eb5f11403c7003cfa
Robson Areias <mirrion@gmail.com>
2019-01-31 08:37:19 -0200

    CRUD Scaffolding

---
+++ ProjetoAsp/Data/ProjetoAspContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoAsp.Models
+{
+    public class ProjetoAspContext : DbContext
+    {
+        public ProjetoAspContext (DbContextOptions<ProjetoAspContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoAsp.Models.Department> Department { get; set; }
+    }
+}
