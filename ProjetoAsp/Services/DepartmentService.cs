24fc4132a1ebbeaa1835db127e0939d56d499938
Robson Areias <mirrion@gmail.com>
2019-02-01 08:31:16 -0200

    SellerFormViewModel and Department select component

---
+++ ProjetoAsp/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoAsp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoAsp.Services
+{
+    public class DepartmentService
+    {
+        private readonly ProjetoAspContext _context;
+
+        public DepartmentService(ProjetoAspContext context)
+        {
+            _context = context;
+        }
+
+       public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
