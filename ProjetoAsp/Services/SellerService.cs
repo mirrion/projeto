0ea0a8b095c201f7ab7fd5d2a8005d2511b44c5d
Robson Areias <mirrion@gmail.com>
2019-01-31 23:08:50 -0200

    SellerService and basic FindAll

---
+++ ProjetoAsp/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using ProjetoAsp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAsp.Services
+{
+    public class SellerService
+    {
+        private readonly ProjetoAspContext _context;
+
+        public SellerService(ProjetoAspContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- ProjetoAsp/Startup.cs
+++ ProjetoAsp/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ProjetoAsp.Models;
 using ProjetoAsp.Data;
+using ProjetoAsp.Services;
 
 namespace ProjetoAsp
 {
@@ -42,6 +43,7 @@ namespace ProjetoAsp
                     builder.MigrationsAssembly("ProjetoAsp")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
