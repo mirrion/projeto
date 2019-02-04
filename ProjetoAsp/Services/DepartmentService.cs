2d4a8e43b289544b4e948adddb2ebb68b8ab85a7
Robson Areias <mirrion@gmail.com>
2019-02-04 18:04:34 -0200

    Exception handling for delete (referential integrity)

--- ProjetoAsp/Services/DepartmentService.cs
+++ ProjetoAsp/Services/DepartmentService.cs
@@ -1,6 +1,8 @@
 ﻿using ProjetoAsp.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoAsp.Services
 {
@@ -13,9 +15,9 @@ namespace ProjetoAsp.Services
             _context = context;
         }
 
-       public List<Department> FindAll()
+       public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
---
+++ ProjetoAsp/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProjetoAsp.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
